c11c358c8bbbd15925bc099c87a785e51fc8eebb
intsuc <intsuc@gmail.com>
2025-12-07 22:41:38 +0900

    Add menu icons

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,30 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
+import { FileMdIcon, GraphIcon } from "@phosphor-icons/react"
 
 const rootElement = document.getElementById("root")!
 
+function Document() {
+  const markdownPathname = location.pathname === "/" ? "/index.md" : `${location.pathname}.md`
+
+  return (
+    <div>
+      <div className="p-1 sticky top-0 flex gap-1 justify-end border-b border-zinc-200 bg-white">
+        <a>
+          <GraphIcon size={20} />
+        </a>
+        <a href={markdownPathname}>
+          <FileMdIcon size={20} />
+        </a>
+      </div>
+      <div dangerouslySetInnerHTML={{ __html: rootElement.innerHTML }} />
+    </div>
+  )
+}
+
 createRoot(rootElement).render(
   <StrictMode>
-    <div dangerouslySetInnerHTML={{ __html: rootElement.innerHTML }} />
+    <Document />
   </StrictMode>,
 )

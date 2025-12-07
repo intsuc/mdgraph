51c00931bdae0f9eec97d316b529849948ce8c61
intsuc <intsuc@gmail.com>
2025-12-08 00:38:39 +0900

    Add translate button

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,24 +1,96 @@
-import { StrictMode } from "react"
+import { StrictMode, useEffect, useState } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
-import { FileMdIcon, GraphIcon } from "@phosphor-icons/react"
+import { FileMdIcon, GraphIcon, TranslateIcon } from "@phosphor-icons/react"
 
 const rootElement = document.getElementById("root")!
+const originalHtml = rootElement.innerHTML
+
+async function checkTranslatorAvailability(text: string): Promise<{
+  sourceLanguage: string,
+  targetLanguage: string,
+} | undefined> {
+  if (!("LanguageDetector" in window)) { return undefined }
+
+  const detector = await LanguageDetector.create()
+  const result = (await detector.detect(text))[0]
+  if (result === undefined) { return undefined }
+
+  const { detectedLanguage: sourceLanguage } = result
+  if (sourceLanguage === undefined) { return undefined }
+  const targetLanguage = navigator.language
+
+  const availability = await Translator.availability({ sourceLanguage, targetLanguage })
+  if (availability !== "available") { return undefined }
+
+  return { sourceLanguage, targetLanguage }
+}
+
+async function translate(text: string): Promise<string | undefined> {
+  if (!("LanguageDetector" in window)) { return undefined }
+
+  const detector = await LanguageDetector.create()
+  const result = (await detector.detect(text))[0]
+  if (result === undefined) { return undefined }
+
+  const { detectedLanguage: sourceLanguage } = result
+  if (sourceLanguage === undefined) { return undefined }
+  const targetLanguage = navigator.language
+
+  const availability = await Translator.availability({ sourceLanguage, targetLanguage })
+  if (availability !== "available") { return undefined }
+
+  const translator = await Translator.create({ sourceLanguage, targetLanguage })
+  const translatedText = await translator.translate(text)
+
+  return translatedText
+}
+
+const text = "Hello, world!"
 
 function Document() {
   const markdownPathname = `${location.pathname}${location.pathname.endsWith("/") ? "index" : ""}.md`
 
+  const [canTranslate, setCanTranslate] = useState(false)
+  useEffect(() => {
+    void (async () => {
+      const availability = await checkTranslatorAvailability(text)
+      setCanTranslate(availability !== undefined)
+    })()
+  }, [])
+
   return (
     <div>
       <div className="p-2 sticky top-0 flex gap-2 justify-end border-b border-zinc-200 bg-white">
-        <a>
+        <button
+          title="Translate"
+          aria-label="Translate"
+          disabled={!canTranslate}
+          onClick={() => {
+            void (async () => {
+              const result = await translate(text)
+              console.log(text, result)
+            })()
+          }}
+          className="disabled:opacity-50 disabled:cursor-not-allowed"
+        >
+          <TranslateIcon size={24} />
+        </button>
+        <a
+          title="View in Graph"
+          aria-label="View in Graph"
+        >
           <GraphIcon size={24} />
         </a>
-        <a href={markdownPathname}>
+        <a
+          title="View in Markdown"
+          aria-label="View in Markdown"
+          href={markdownPathname}
+        >
           <FileMdIcon size={24} />
         </a>
       </div>
-      <div className="mx-auto px-4 py-8 prose prose-zinc" dangerouslySetInnerHTML={{ __html: rootElement.innerHTML }} />
+      <div className="mx-auto px-4 py-8 prose prose-zinc" dangerouslySetInnerHTML={{ __html: originalHtml }} />
     </div>
   )
 }

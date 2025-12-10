4c316e24388fe53dfaac5526ef95594c1be366bf
intsuc <intsuc@gmail.com>
2025-12-10 20:53:35 +0900

    Use shadcn buttons

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode, useEffect, useState } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
+import { LanguagesIcon, FileDownIcon, PictureInPictureIcon } from "lucide-react"
+import { Button } from "@/components/ui/button"
 
 const rootElement = document.getElementById("root")!
 const originalText = rootElement.innerText
@@ -23,11 +25,9 @@ async function summarize(text: string): Promise<ReadableStream<string> | undefin
   }
 }
 
-async function checkTranslatorAvailability(text: string): Promise<{
-  sourceLanguage: string,
-  targetLanguage: string,
-} | undefined> {
+async function checkTranslatorAvailability(text: string): Promise<TranslatorCreateCoreOptions | undefined> {
   if (!("LanguageDetector" in window)) { return undefined }
+  if (!("Translator" in window)) { return undefined }
 
   const detector = await LanguageDetector.create()
   const result = (await detector.detect(text))[0]
@@ -43,99 +43,74 @@ async function checkTranslatorAvailability(text: string): Promise<{
   return { sourceLanguage, targetLanguage }
 }
 
-async function translate(text: string): Promise<string | undefined> {
-  if (!("LanguageDetector" in window)) { return undefined }
-
-  const detector = await LanguageDetector.create()
-  const result = (await detector.detect(text))[0]
-  if (result === undefined) { return undefined }
-
-  const { detectedLanguage: sourceLanguage } = result
-  if (sourceLanguage === undefined) { return undefined }
-  const targetLanguage = navigator.language
-
-  const availability = await Translator.availability({ sourceLanguage, targetLanguage })
-  if (availability === "unavailable") { return undefined }
-
-  const translator = await Translator.create({ sourceLanguage, targetLanguage })
+async function translate(text: string, options: TranslatorCreateCoreOptions): Promise<string | undefined> {
+  const translator = await Translator.create(options)
   const translatedText = await translator.translate(text)
-
   return translatedText
 }
 
 function Document() {
   const markdownPathname = `${location.pathname}${location.pathname.endsWith("/") ? "index" : ""}.md`
 
-  const [summary, setSummary] = useState<string | undefined>(undefined)
-
-  const [canTranslate, setCanTranslate] = useState(false)
+  const [translatorOptions, setTranslatorOptions] = useState<TranslatorCreateCoreOptions | undefined>(undefined)
   useEffect(() => {
     void (async () => {
-      const availability = await checkTranslatorAvailability(originalText)
-      setCanTranslate(availability !== undefined)
+      const translatorOptions = await checkTranslatorAvailability(originalText)
+      setTranslatorOptions(translatorOptions)
     })()
   }, [])
 
   return (
     <div>
       <div className="p-2 sticky top-0 flex gap-2 justify-end border-b border-zinc-200 bg-white">
-        <button
+        <Button
+          variant="outline"
+          size="icon"
           title="Translate"
           aria-label="Translate"
-          disabled={!canTranslate}
+          disabled={translatorOptions === undefined}
           onClick={() => {
             void (async () => {
-              const result = await translate(originalText)
+              if (translatorOptions === undefined) { return }
+              const result = await translate(originalText, translatorOptions)
               console.log(originalText)
               console.log(result)
             })()
           }}
           className="disabled:opacity-50 disabled:cursor-not-allowed"
         >
-          <TranslateIcon size={24} />
-        </button>
-        <a
-          title="View in Graph"
-          aria-label="View in Graph"
+          <LanguagesIcon />
+        </Button>
+        <Button
+          variant="outline"
+          size="icon"
+          asChild
         >
-          <GraphIcon size={24} />
-        </a>
-        <a
-          title="View in Markdown"
-          aria-label="View in Markdown"
-          href={markdownPathname}
-        >
-          <FileMdIcon size={24} />
-        </a>
-      </div>
-      <div className="mx-auto px-4 py-8 prose prose-zinc">
-        <div className="mb-4 p-2 border border-zinc-300 rounded bg-zinc-50">
-          <button
-            title="Summarize"
-            aria-label="Summarize"
-            onClick={() => {
-              setSummary(undefined)
-              void (async () => {
-                const summary = await summarize(originalText)
-                if (summary !== undefined) {
-                  const reader = summary.getReader()
-                  while (true) {
-                    const { done, value } = await reader.read()
-                    if (done) { break }
-                    setSummary((prev) => (prev ?? "") + (value ?? ""))
-                  }
-                }
-              })()
-            }}
+          <a
+            title="View in Graph"
+            aria-label="View in Graph"
           >
-            <SparkleIcon size={24} />
-          </button>
-          {summary !== undefined ? (
-            <div className="text-left">{summary}</div>
-          ) : null}
-        </div>
-        <div dangerouslySetInnerHTML={{ __html: originalHtml }} />
+            <PictureInPictureIcon size={24} />
+          </a>
+        </Button>
+        <Button
+          variant="outline"
+          size="icon"
+          asChild
+        >
+          <a
+            title="View in Markdown"
+            aria-label="View in Markdown"
+            href={markdownPathname}
+          >
+            <FileDownIcon size={24} />
+          </a>
+        </Button>
       </div>
+      <div
+        className="mx-auto px-4 py-8 prose prose-zinc"
+        dangerouslySetInnerHTML={{ __html: originalHtml }}
+      />
     </div>
   )
 }
